ed66432090adf3cbc6fa2f82dbc0dae5031db3b9
Pqtato <29719362+Kore-Development@users.noreply.github.com>
2019-11-28 17:55:13 -0600

    Create goldenbird.user.js

---
+++ goldenbird.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        Golden Bird
+// @namespace   Violentmonkey Scripts
+// @match       *://pqtato.pw/game/7
+// @match       *://*.pqtato.pw/game/7
+// @grant       none
+// @version     1.0.0
+// @author      Pqtato
+// @description Golden bird for https://pqtato.pw/game/7
+// ==/UserScript==
+
+document.getElementById("offline-resources-1x").src = "https://files.pqtato.pw/download.php?id=15&token=sYj8orAKp3IjElfadM6s3lRyOdjUHr1w&download"
+document.getElementById("offline-resources-2x").src = "https://files.pqtato.pw/download.php?id=16&token=3O87b8ptuxMNmYfkxvEvb8KJzkjYNcKY&download"
